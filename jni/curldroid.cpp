e323b7e1aaadd834da5255956177bccaca9c4324
javamonk <truncatei@gmail.com>
2015-02-10 15:34:55 +0800

    change logger name in jni. rebuild libs

--- jni/curldroid.cpp
+++ jni/curldroid.cpp
@@ -6,7 +6,7 @@
 #include "curl/curl.h"
 #include "curldroid.h"
 
-#define TAG "libcurldroid"
+#define TAG "libcurldroid.jni"
 #define LOGW(...) (__android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__))
 #define LOGD(...) (__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__))
 #define LOGV(...) (__android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__))
